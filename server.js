10456be4879ed3b57ded5dcbaa6f483f2309a083
Gabriel Bell <gmbell23@gmail.com>
2017-03-24 22:35:36 +0000

    change to server

--- server.js
+++ server.js
@@ -6,17 +6,18 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'/index.html'));
 });
 
-//input examples:
-// December%1%2015,%202015
-//1450137600
+//input examples: December%1%2015,%202015     1450137600
 
-app.get('/app', function(req,res){
+app.get('/api', function(req,res){
     
     var url= parse(req.url)
     console.log(url);
     res.sendFile(path.join(__dirname,'/landing.html'))
-    
 });
+
+app.get('*', function(req,res){
+    res.end("404! sorry");
+})
 
 app.listen(process.env.PORT, function(){
     console.log("app running on port 8080")
