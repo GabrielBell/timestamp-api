c468bb37e1e564fc7f3830daf444ea9a488cd369
Gabriel Bell <gmbell23@gmail.com>
2017-03-24 21:55:23 +0000

    testing landing page

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'/index.html'));
 });
 
+//input examples:
+// December%1%2015,%202015
+//1450137600
+
+app.get('/app/:timestamp', function(req,res){
+    //access req.params.timestamp
+    console.log(req.params.timestamp)
+    res.send("Finished")
+    
+})
+
 app.listen(process.env.PORT || 8080, function(){
     console.log("app running on port 8080")
 })
