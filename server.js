7e50ba956b28d907e7c5b956d724608bd7753d7f
Gabriel Bell <gmbell23@gmail.com>
2017-03-24 22:13:51 +0000

    landing2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express= require("express")
 var app= express()
-var path= require("path")
+var path= require("path"), parse= require("url-parse")
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'/index.html'));
@@ -10,13 +10,14 @@ app.get('/', function(req,res){
 // December%1%2015,%202015
 //1450137600
 
-app.get('/app/:timestamp', function(req,res){
-    //access req.params.timestamp
-    console.log(req.params.timestamp)
+app.get('/app', function(req,res){
+    
+    var url= parse(req.url)
+    console.log(url);
     res.send("Finished")
     
 })
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(process.env.PORT, function(){
     console.log("app running on port 8080")
 })
