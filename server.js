f15b8914c7a917330b728a903753262ce2ca74eb
Gabriel Bell <gmbell23@gmail.com>
2017-03-25 00:12:31 +0000

    json output

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express= require("express")
 var app= express()
-var path= require("path"), parse= require("url-parse")
+var path= require("path"), parse= require("url-parse"), unix, standard;
+var dateFormat= require('dateformat')
+
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'/index.html'));
@@ -10,9 +12,13 @@ app.get('/', function(req,res){
 
 app.get('/api', function(req,res){
     
-    var url= parse(req.url)
-    console.log(url);
-    res.json(url)
+    var timeF=url2Time(parse(req.url))
+    if(timeF===null) res.send("invalid date string")
+    unix= timeF.getTime()/1000;
+    standard= dateFormat(timeF, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+    
+    
+    res.json({'unix': unix, 'natural': standard})
 });
 
 app.get('*', function(req,res){
@@ -22,3 +28,12 @@ app.get('*', function(req,res){
 app.listen(process.env.PORT, function(){
     console.log("app running on port 8080")
 })
+
+function url2Time(url){
+    var timeStr= url.query
+    var timeForm= timeStr.replace(/[^A-Za-z0-9/-]+/g, " ");
+    if(isNaN(Date.parse(timeForm))){
+        return null
+    }
+    return new Date(timeForm)
+}
